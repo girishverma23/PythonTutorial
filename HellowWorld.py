2448ae67cc8bc88133956f192e26e451dd73706f
Girish Verma <girish.gcet@gmail.com>
2015-06-28 20:19:30 -0700

    Data Type Number int,float,long,complex

--- HellowWorld.py
+++ HellowWorld.py
@@ -11,3 +11,19 @@ name = "Rahul!"
 age  = 25
 # printf type printing
 print("Name = %s Agne : %d" % (name, age))
+
+'''
+    Python Built-in Data Types
+    1. Number : int, float, long, complex
+    2. String : str
+    3. List   : list
+    4. Tuples : tuple
+    5. Dict/map
+'''
+int_num = int(23.5)
+float_num = float(23.5)
+long_num  = long(0xffffffffffffffff)
+complex_num = complex(5,4);
+
+print(int_num, float_num, long_num, complex_num);
+
