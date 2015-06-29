4954565cb9fe1addec6fd3e5e2a88b043245b13d
Girish Verma <girish.gcet@gmail.com>
2015-06-28 20:52:01 -0700

    mutiple line string , List, tuple

--- HellowWorld.py
+++ HellowWorld.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # /usr/bin/env : Find correct location and version of python as per PATH
 
+# https://www.youtube.com/watch?v=N4mEzFDjqtA
 
 ''' To use Multiple Line Comment
     use 3'
@@ -15,9 +16,9 @@ print("Name = %s Agne : %d" % (name, age))
 '''
     Python Built-in Data Types
     1. Number : int, float, long, complex
-    2. String : str
-    3. List   : list
-    4. Tuples : tuple
+    2. String : str, unicode
+    3. List   : list []
+    4. Tuples : tuple ()
     5. Dict/map
 '''
 int_num = int(23.5)
@@ -29,3 +30,19 @@ print(int_num, float_num, long_num, complex_num);
 
 print( "5.1 // 2 (floored) integer division  x and y = " , (5.1 // 2))
 print( "5.1 / 2 division of x and y = " , (5.1 / 2))
+
+str_name = str("hi this is string")
+str_multiline = ''' hi this is multiple
+                    Wow
+                '''
+print(str_multiline)
+unicode_name = unicode("Hi  this is unicode name")
+
+print(str_name)
+print(unicode_name)
+
+list_num = [1,2,3,4,5]
+tuple_num = (1,2,3,4,5)
+
+print(list_num)
+print(tuple_num)
